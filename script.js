7d4d8ed1762b710114af49f9de54bb14186327ce
Renato Viturino <renatoviturino80@gmail.com>
2024-11-27 23:05:13 -0300

    Ajustes na sintaxe do código

--- script.js
+++ script.js
@@ -18,21 +18,16 @@ function Get(digits, verifiers) {
 
 Get.prototype.findVerifiers = function () {
     const ARRAY_DIGITS = Array.from(this.digits);
-
     const MULTIPLY_DIGITS = ARRAY_DIGITS.map((value, index) => this.digits.length === 9 ? Number(value) * (index + 1) : Number(value) * (index));
     const SUM_DIGITS = MULTIPLY_DIGITS.reduce((acumulator, value) => acumulator += value);
 
     (SUM_DIGITS % 11) >= 10 ? this.digits += '0' : this.digits += String(SUM_DIGITS % 11);
 
-    if (this.digits.length === 10) {
-        this.findVerifiers();
-    }
+    if (this.digits.length === 10) this.findVerifiers();
 }
 
 Get.prototype.validate = function () {
-    if (this.digits.length !== 9 || this.verifiers.length !== 2) {
-        throw new Error("CPF inválido");
-    }
+    if (this.digits.length !== 9 || this.verifiers.length !== 2) throw new Error("CPF inválido");
 
     this.findVerifiers();
 
